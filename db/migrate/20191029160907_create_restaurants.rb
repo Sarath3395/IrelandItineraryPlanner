0ed88970fefafde0bda135b76eb38f49a354d180
Sarath3395 <Sarath3395@users.noreply.github.com>
2019-10-29 17:37:13 +0000

    Update 20191029160907_create_restaurants.rb

--- db/migrate/20191029160907_create_restaurants.rb
+++ db/migrate/20191029160907_create_restaurants.rb
@@ -2,7 +2,7 @@ class CreateRestaurants < ActiveRecord::Migration[6.0]
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.string :food_type
+      t.string :type
       t.string :price
       t.references :location, null: false, foreign_key: true
 
