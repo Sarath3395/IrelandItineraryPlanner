02c290f9b6c1d43d0f23b3161ea25d36e18f779a
Sarath3395 <Sarath3395@users.noreply.github.com>
2019-10-29 17:43:56 +0000

    updated

--- db/migrate/20191029162528_change_column_name_restaurants.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnNameRestaurants < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :restaurants, :type, :foodtype
-  end
-end
--- db/migrate/20191029164205_change_column_name_transports.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnNameTransports < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :transports, :type, :transporttype
-  end
-end
