280cd2694f7f1cb7532d39c9aff7288541dfe46b
Sarath3395 <Sarath3395@users.noreply.github.com>
2019-12-06 23:11:24 +0000

    update

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,4 +1,6 @@
 class CallbacksController < ApplicationController
+  protect_from_forgery with: :exception
+
   def google_oauth2
 @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user
